e563be0206961641fe9e0b63d3fbd9bca967137e
Mark Grey <mark@mark-MS-7642.(none)>
2013-02-07 14:38:36 -0500

    initial

--- nintendocs/__init__.py
+++ nintendocs/__init__.py
@@ -3,12 +3,13 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write("Hello, wrold!")
 
-application = tornado.web.Application([
+url_map = [
     (r"/", MainHandler),
-])
+]
 
 if __name__ == "__main__":
+    application = tornado.web.Application(url_map,debug=True)
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
--- nintendocs/__main__.py
+++ nintendocs/__main__.py
@@ -1,5 +1,7 @@
-from nintendocs import application
-import tornado.ioloop
+from nintendocs import url_map
+import tornado.web
 
+application = tornado.web.Application(url_map,debug=True)
 application.listen(8888)
 tornado.ioloop.IOLoop.instance().start()
+
